01254060cd5afbb78737566be501e1ba521f9c9b
pitalerushikesh <pitalerushikesh@gmail.com>
2024-05-08 11:04:35 +0530

    Update CORS whitelist for stability support

--- index.ts
+++ index.ts
@@ -1,39 +1,9 @@
-// import express, { Request, Response, NextFunction } from "express";
-// import cors from "cors";
-// import serverless from "serverless-http";
-// import bodyParser from "body-parser";
-// import { corsOrigin } from "./lambda/utils";
-// import { ALLOWED_ORIGINS, MAX_REQUEST_BODY_SIZE } from "./lambda/constants";
-// import { router as storageRouter } from "./lambda/functions/storage/router";
-// import { router as verifyRouter } from "./lambda/functions/verify/router";
-
-// const app = express();
-
-// app.use(cors({ origin: corsOrigin }));
-// app.use(bodyParser.json({ limit: MAX_REQUEST_BODY_SIZE }));
-// app.use(
-//   bodyParser.urlencoded({ limit: MAX_REQUEST_BODY_SIZE, extended: true })
-// );
-// // app.use((req: Request, res: Response, next: NextFunction) => {
-// //   res.append("Access-Control-Allow-Origin", "*");
-// //   next();
-// // });
-// app.use("/functions/storage", storageRouter);
-// app.use("/functions/verify", verifyRouter);
-// app.disable("x-powered-by");
-
-// export const handler = serverless(app);
-
-// // app.listen(9999, () => {
-// //   console.log("Server running on port 9999");
-// // });
-
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 import serverless from "serverless-http";
 import bodyParser from "body-parser";
 import { corsOrigin } from "./lambda/utils";
-import { MAX_REQUEST_BODY_SIZE } from "./lambda/constants";
+import { ALLOWED_ORIGINS, MAX_REQUEST_BODY_SIZE } from "./lambda/constants";
 import { router as storageRouter } from "./lambda/functions/storage/router";
 import { router as verifyRouter } from "./lambda/functions/verify/router";
 
@@ -42,14 +12,18 @@ const app = express();
 app.use(cors({ origin: corsOrigin }));
 app.use(bodyParser.json({ limit: MAX_REQUEST_BODY_SIZE }));
 app.use(
-	bodyParser.urlencoded({ limit: MAX_REQUEST_BODY_SIZE, extended: true })
+  bodyParser.urlencoded({ limit: MAX_REQUEST_BODY_SIZE, extended: true })
 );
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//   res.append("Access-Control-Allow-Origin", "*");
+//   next();
+// });
 app.use("/functions/storage", storageRouter);
 app.use("/functions/verify", verifyRouter);
-// app.disable("x-powered-by");
+app.disable("x-powered-by");
 
-// export const handler = serverless(app);
+export const handler = serverless(app);
 
-app.listen(8888, () => {
-	console.log("Server running on port 8888");
-});
+// app.listen(9999, () => {
+//   console.log("Server running on port 9999");
+// });
